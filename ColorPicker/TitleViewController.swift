c46956c4b1a955e580255050bcd5fa83836e0d72
Kazuya <shiozaki5621@gmail.com>
2019-04-21 15:26:38 -0700

    add Splash Screen

---
+++ ColorPicker/TitleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TitleViewController.swift
+//  ColorPicker
+//
+//  Created by SubaruShiozaki on 2019-04-20.
+//  Copyright © 2019 Kazuya Takahashi. All rights reserved.
+//
+
+import UIKit
+
+class TitleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
